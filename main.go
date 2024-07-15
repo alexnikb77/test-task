ada95c57d6d19801b2e32d40bac48270fcd45888
alexnikb77 <alexnikb@gmail.com>
2024-07-15 16:55:37 +0300

    Исправление согласно комментариям к ТЗ

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 )
 
 var a []string
-var k, p int
+var k, p, l, f int
 var z float64
-var x, y string
+var x, y, q string
 
 func delSpaces(str string) string {
 	result := make([]rune, 0, len(str))
@@ -59,7 +59,7 @@ func findOper(st string) int {
 func mathcalc(one, two string) float64 {
 
 	var res, one1, two1 float64
-	var f, x, y int
+	var r, o int
 	switch one {
 	case "I":
 		one1 = 1
@@ -79,6 +79,8 @@ func mathcalc(one, two string) float64 {
 		one1 = 8
 	case "IX":
 		one1 = 9
+	case "X":
+		one1 = 10
 	default:
 		f = 1
 	}
@@ -101,6 +103,8 @@ func mathcalc(one, two string) float64 {
 		two1 = 8
 	case "IX":
 		two1 = 9
+	case "X":
+		two1 = 10
 	default:
 		f = 1
 	}
@@ -122,10 +126,10 @@ func mathcalc(one, two string) float64 {
 			panic("По тз пример с римскими цифрами не может быть отридцательным и равным нулю")
 		}
 	} else {
-		x, _ = strconv.Atoi(one)
-		y, _ = strconv.Atoi(two)
-		one1 = float64(x)
-		two1 = float64(y)
+		r, _ = strconv.Atoi(one)
+		o, _ = strconv.Atoi(two)
+		one1 = float64(r)
+		two1 = float64(o)
 
 		if one1 < 1 || two1 < 1 {
 			//fmt.Println("пример не соответствует шаблону")
@@ -148,6 +152,7 @@ func mathcalc(one, two string) float64 {
 			res = float64(one1 / two1)
 		}
 	}
+
 	return res
 }
 
@@ -179,7 +184,214 @@ func main() {
 		}
 
 		z = mathcalc(a[0], a[1])
-		fmt.Println(z)
+		l = int(z)
+		if f == 0 {
+			switch l {
+			case 1:
+				q = "I"
+			case 2:
+				q = "II"
+			case 3:
+				q = "III"
+			case 4:
+				q = "IV"
+			case 5:
+				q = "V"
+			case 6:
+				q = "VI"
+			case 7:
+				q = "VII"
+			case 8:
+				q = "VIII"
+			case 9:
+				q = "IX"
+			case 10:
+				q = "X"
+			case 11:
+				q = "XI"
+			case 12:
+				q = "XII"
+			case 13:
+				q = "XIII"
+			case 14:
+				q = "XIV"
+			case 15:
+				q = "XV"
+			case 16:
+				q = "XVI"
+			case 17:
+				q = "XVII"
+			case 18:
+				q = "XVIII"
+			case 19:
+				q = "IX"
+			case 20:
+				q = "XX"
+			case 21:
+				q = "XXI"
+			case 22:
+				q = "XXII"
+			case 23:
+				q = "XXIII"
+			case 24:
+				q = "XXIV"
+			case 25:
+				q = "XXV"
+			case 26:
+				q = "XXVI"
+			case 27:
+				q = "XXVII"
+			case 28:
+				q = "XXVIII"
+			case 29:
+				q = "XXIX"
+			case 30:
+				q = "XXX"
+			case 31:
+				q = "XXXI"
+			case 32:
+				q = "XXXII"
+			case 33:
+				q = "XXXIII"
+			case 34:
+				q = "XXXIV"
+			case 35:
+				q = "XXXV"
+			case 36:
+				q = "XXXVI"
+			case 37:
+				q = "XXXVII"
+			case 38:
+				q = "XXXVIII"
+			case 39:
+				q = "XXXIX"
+			case 40:
+				q = "XL"
+			case 41:
+				q = "XLI"
+			case 42:
+				q = "XLII"
+			case 43:
+				q = "XLIII"
+			case 44:
+				q = "XLIV"
+			case 45:
+				q = "XLV"
+			case 46:
+				q = "XLVI"
+			case 47:
+				q = "XLVII"
+			case 48:
+				q = "XLVIII"
+			case 49:
+				q = "XLIX"
+			case 50:
+				q = "L"
+			case 51:
+				q = "LI"
+			case 52:
+				q = "LII"
+			case 53:
+				q = "LIII"
+			case 54:
+				q = "LIV"
+			case 55:
+				q = "LV"
+			case 56:
+				q = "LVI"
+			case 57:
+				q = "LVII"
+			case 58:
+				q = "LVIII"
+			case 59:
+				q = "LVIX"
+			case 60:
+				q = "LX"
+			case 61:
+				q = "LXI"
+			case 62:
+				q = "LXII"
+			case 63:
+				q = "LXIII"
+			case 64:
+				q = "LXIV"
+			case 65:
+				q = "LXV"
+			case 66:
+				q = "LXVI"
+			case 67:
+				q = "LXVII"
+			case 68:
+				q = "LXVIII"
+			case 69:
+				q = "LXIX"
+			case 70:
+				q = "LXX"
+			case 71:
+				q = "LXXI"
+			case 72:
+				q = "LXXII"
+			case 73:
+				q = "LXXIII"
+			case 74:
+				q = "LXXIV"
+			case 75:
+				q = "LXXV"
+			case 76:
+				q = "LXXVI"
+			case 77:
+				q = "LXXVII"
+			case 78:
+				q = "LXXVIII"
+			case 79:
+				q = "LXXIX"
+			case 80:
+				q = "LXXX"
+			case 81:
+				q = "LXXXI"
+			case 82:
+				q = "LXXXII"
+			case 83:
+				q = "LXXXIII"
+			case 84:
+				q = "LXXXIV"
+			case 85:
+				q = "LXXXV"
+			case 86:
+				q = "LXXXVI"
+			case 87:
+				q = "LXXXVII"
+			case 88:
+				q = "LXXXVIII"
+			case 89:
+				q = "LXXIX"
+			case 90:
+				q = "XC"
+			case 91:
+				q = "XCI"
+			case 92:
+				q = "XCII"
+			case 93:
+				q = "XCIII"
+			case 94:
+				q = "XCIV"
+			case 95:
+				q = "XCV"
+			case 96:
+				q = "XCVI"
+			case 97:
+				q = "XCVII"
+			case 98:
+				q = "XCVIII"
+			case 99:
+				q = "XCVIX"
+			case 100:
+				q = "C"
+			}
+			fmt.Println(q)
+		} else {
+			fmt.Println(l)
+		}
 
 	}
 
